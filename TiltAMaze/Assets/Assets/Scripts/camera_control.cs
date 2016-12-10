16c015450617d5d08e9b0f2d3197f8e55efce0e2
Jvargasw <jvargasw@uoregon.edu>
2016-12-10 15:26:01 -0800

    Added camera movement (follows player)
    ^^^^

---
+++ TiltAMaze/Assets/Assets/Scripts/camera_control.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_control : MonoBehaviour {
+
+    private Transform player;
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 pPosition = player.position;
+        pPosition.z = transform.position.z;
+        transform.position = pPosition;
+	}
+}
