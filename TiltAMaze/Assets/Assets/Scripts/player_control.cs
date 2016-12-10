8743e9b6c6a7c4acbf011e5f1b127d22a908cf78
Jvargasw <jvargasw@uoregon.edu>
2016-12-09 20:39:49 -0800

    Started building test room + fixed folders
    ^^^

---
+++ TiltAMaze/Assets/Assets/Scripts/player_control.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_control : MonoBehaviour
+{
+    public Sprite player_idle;
+    private SpriteRenderer sRenderer;
+	// Use this for initialization
+	void Start ()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+        if (sRenderer.sprite == null)
+        {
+            sRenderer.sprite = player_idle; //set to idle sprite if no sprite is found
+        } 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
