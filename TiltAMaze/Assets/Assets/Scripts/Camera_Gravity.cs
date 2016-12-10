0a0465d690c929e38cb512a77dd85c17abb633e8
Jvargasw <jvargasw@uoregon.edu>
2016-12-09 22:36:35 -0800

    First prototype
    -gravity rotation with camera
    -player gravity and collision

---
+++ TiltAMaze/Assets/Assets/Scripts/Camera_Gravity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Gravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            transform.Rotate(0,0, 90);
+        }
+
+
+        Physics2D.gravity = -transform.up * 9.82f;
+
+	}
+}
