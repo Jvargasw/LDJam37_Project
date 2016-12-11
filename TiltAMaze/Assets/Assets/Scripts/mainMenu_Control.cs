7cdfdf2a4be6019817075548f02a3ccc3cd71a4b
Jvargasw <jvargasw@uoregon.edu>
2016-12-10 19:08:54 -0800

    Added basic start menu
    ^^^^

---
+++ TiltAMaze/Assets/Assets/Scripts/mainMenu_Control.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu_Control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+    //play button
+    public void PlayPress()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ExitPress()
+    {
+        Application.Quit();
+    }
+}
